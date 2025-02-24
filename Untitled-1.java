89f697b927a0695bd4658a09b545f509b3788ea4
keylock <2102581069@qq.com>
2025-02-24 18:05:26 +0800

    ain

---
+++ Untitled-1.java
@@ -0,0 +1,22 @@
+class Slice_o_Heaven:
+    def __init__(self):
+        self.storeName = ""
+        self.storeAddress = ""
+        self.storeEmail = ""
+        self.storePhone = ""
+        self.storeMenu = []
+        self.pizzaIngredients = []
+        self.pizzaPrice = 0
+        self.sides = []
+        self.drinks = []
+        self.orderID = 0
+        self.orderTotal = 0
+
+    def takeOrder(self):
+        print("Taking order...")
+
+    def makePizza(self):
+        print("Making pizza...")
+
+    def printReceipt(self):
+        print("Printing receipt...")
